7d9e246bf22a1419ab7f054ee377f93aaa6209cb
aditya2604 <aditya26042005@gmail.com>
2021-08-20 22:43:22 -0700

    status cog

--- cogs/status.py
+++ cogs/status.py
@@ -17,7 +17,7 @@ class Status(commands.Cog):
         total_members = 0
         for guild in self.bot.guilds:
             total_members += guild.member_count
-        self.status = ["for this life i cannot change", 'elmo complaining', f"{total_members} losers"] # ,help | @Kermit, f"on {len(self.bot.guilds)} servers"
+        self.status = ["VALORANT", 'Tucker Klansman', f"{total_members} babygorls"] # ,help | @Kermit, f"on {len(self.bot.guilds)} servers"
 
         self.activity = cycle([discord.Game(name=self.status[0]), discord.Activity(type=discord.ActivityType.listening,
                         name=(self.status[1])), discord.Activity(type=discord.ActivityType.watching, name=(self.status[2]))])
