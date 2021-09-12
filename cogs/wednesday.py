15536b7bd05319197b8bf14e6f8b671ac88cef57
aditya2604 <aditya26042005@gmail.com>
2021-09-12 13:58:27 -0700

    continuing the apple tradition ong

--- cogs/wednesday.py
+++ cogs/wednesday.py
@@ -55,6 +55,12 @@ class Wednesday(commands.Cog):
                     await self.channel.send(file=discord.File('media/wednesday_pic.png'))
                 except:
                     pass
+        if self.time().hour == 3:
+            try:
+                self.channel = await self.bot.fetch_channel(766897690952728626)
+                await self.channel.send(":/")
+            except:
+                pass
     
     @commands.Cog.listener()
     async def on_ready(self):
