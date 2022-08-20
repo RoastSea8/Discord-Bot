e5595aa074baef58688cedb4e38dfda5f3256ad5
aditya2604 <aditya26042005@gmail.com>
2022-08-20 14:59:30 -0700

    use my urbdict dependency

--- cogs/define.py
+++ cogs/define.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands 
-import requests
-from bs4 import BeautifulSoup
+import urbdict
 
 class Define(commands.Cog):
 
@@ -12,26 +11,14 @@ class Define(commands.Cog):
     async def define(self, ctx, *, words):
         words = words.lower()
         words = words.replace(' ', '%20')
-        r = requests.get(f"http://www.urbandictionary.com/define.php?term={words}")
-        soup = BeautifulSoup(r.content, 'lxml')
         try:
-            current_url = r.url.replace('%20', '+')
-            current_url = current_url[current_url.rfind('/'):]
-            def_header = soup.find("a", attrs={"href": current_url}).text
-            meaning = soup.find("div", attrs={"class": "break-words meaning mb-4"}).text
-            for br in soup.find_all("br"):
-                br.replace_with("\n")
-            example = soup.find("div", attrs={"class": "break-words example italic mb-4"}).text
-            contributor = soup.find("div", attrs={"class": "contributor font-bold"}).text
-            up_votes = soup.find("span", attrs={"data-x-text": "thumbText('up')"}).text
-            down_votes = soup.find("span", attrs={"data-x-text": "thumbText('down')"}).text
+            word_dict = urbdict.define(words)
             embed = discord.Embed(
-                title=f"{def_header}",
-                url=f"http://www.urbandictionary.com/define.php?term={words}",
-                description=f'**Definition**: {meaning}\n\n**Example**:\n*{example}*\n\n{contributor}'
+                title=word_dict["word"],
+                url=word_dict["url"],
+                description=f'**Definition**: {word_dict["definition"]}\n\n**Example**:\n*{word_dict["example"]}*\n\n{word_dict["contributor"]}'
             )
             embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-            embed.set_footer(text=f"👍 {up_votes} | 👎 {down_votes}")
             await ctx.send(embed=embed)
         except:
             words = words.replace(' ', '%20')
